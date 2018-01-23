3dd9da2fedec784ba7cfd7a6a791245336b8cfb4
Fandia <cgfandia@gmail.com>
2018-01-24 00:06:10 +0300

    states slider, play buttons

--- algorithms/algo.py
+++ algorithms/algo.py
@@ -4,8 +4,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class AlgoWidget(QtWidgets.QWidget):
-    __metaclass__ = ABCMeta
-    @abstractmethod
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -18,6 +18,9 @@ MIN_RAND = 1
 MAX_RAND = 10
 ELEMENTS_COUNT = 20
 
+ELEMENT_COLOR = QtGui.QColor(0,128,255)
+SELECT_COLOR = QtGui.QColor(255,190,0)
+
 BUBBLE_SORT_STRING = "Сортировка пузырьком"
 QUICK_SORT_STRING = "Быстрая сортировка"
 BUBBLE_SORT_DESCR = r"""
@@ -76,18 +79,30 @@ class StateElement():
 
 
 class SortWidget(AlgoWidget):
-    __metaclass__ = ABCMeta
-    @abstractmethod
     def __init__(self, min_rand, max_rand, elements_count, parent=None):
         super().__init__(parent)
         self.main_layout = QtWidgets.QVBoxLayout()
+        self.play_layout = QtWidgets.QHBoxLayout()
         self.descr_layout = QtWidgets.QHBoxLayout()
         self.sort_graphic_scene = QtWidgets.QGraphicsScene()
         self.sort_graphic_view = QtWidgets.QGraphicsView(self.sort_graphic_scene)
-        self.control_button = QtWidgets.QPushButton("Play - Pause", self)
-        self.control_button.clicked.connect(self.change_play_states_wait_event)
-        self.sort_list = []
+        #   Play buttons
+        self.play_button = QtWidgets.QPushButton("Play - Pause", self)
+        self.play_button.clicked.connect(self.change_play_states_wait_event)
+        self.next_button = QtWidgets.QPushButton("Next", self)
+        self.next_button.clicked.connect(self.next_state)
+        self.prev_button = QtWidgets.QPushButton("Previous", self)
+        self.prev_button.clicked.connect(self.prev_state)
+        self.play_layout.addWidget(self.prev_button)
+        self.play_layout.addWidget(self.play_button)
+        self.play_layout.addWidget(self.next_button)
+        #   States slider
+        self.states_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
+        self.states_slider.valueChanged.connect(self.slider_changed)
+        self.states_slider.sliderPressed.connect(self.slider_pressed)
+        self.states_slider.sliderReleased.connect(self.slider_released)
 
+        self.sort_list = []
         self.states_list = []
         self.current_state = 0
         self.max_state = 0
@@ -97,8 +112,8 @@ class SortWidget(AlgoWidget):
 
         self.max_value = max_rand
         self.values_range = max(abs(max_rand - min_rand), abs(max_rand), abs(min_rand))
-        self.select_color = QtGui.QColor(150,250,0)
-        self.standard_color = QtGui.QColor(200,200,0)
+        self.select_color = SELECT_COLOR
+        self.standard_color = ELEMENT_COLOR
 
         if(min_rand >= 0 and max_rand >= 0):
             self.ground_level = self.sort_graphic_view.height()
@@ -112,13 +127,16 @@ class SortWidget(AlgoWidget):
             element.setBrush(self.standard_color)
             self.sort_list.append(element)
             self.sort_graphic_scene.addItem(self.sort_list[-1])
-        #self.sort_list.sort()
-        #self.update_elements(None)
+
+        self.set_states()
+        self.states_slider.setMaximum(self.max_state)
+        self.states_slider.setMinimum(0)
 
         self.main_layout.addWidget(self.sort_graphic_view)
-        self.main_layout.addWidget(self.control_button)
+        self.main_layout.addWidget(self.states_slider)
+        self.main_layout.addLayout(self.play_layout)
         self.main_layout.addLayout(self.descr_layout)
-        self.resizeEvent = self.update_elements
+        self.resizeEvent = self.update_diagramm
         self.setLayout(self.main_layout)
         self.resize(SORT_WIN_WIDTH, SORT_WIN_HEIGHT)
 
@@ -128,7 +146,7 @@ class SortWidget(AlgoWidget):
         self.descr_layout.addWidget(descr_label)
         self.descr_layout.addWidget(alg_label)
 
-    def update_elements(self, event):
+    def update_diagramm(self, event):
         width = self.sort_graphic_view.width() * 0.04
         for i, element in enumerate(self.sort_list, 0):
             height = abs(element.value) / self.values_range * self.sort_graphic_view.height()
@@ -144,24 +162,24 @@ class SortWidget(AlgoWidget):
         self.sort_graphic_scene.setSceneRect(new_rect)
         self.sort_graphic_view.fitInView(new_rect)
 
-    def swap_elements(self, first, second):
-        self.sort_list[first], self.sort_list[second] = self.sort_list[second], self.sort_list[first]
-
-    @abstractmethod
     def set_states(self):
         pass
 
-    def timerEvent(self, event=None):
+    def set_by_states(self):
         for i, state_element in enumerate(self.states_list[self.current_state], 0):
             self.sort_list[i].value = state_element.value
             self.sort_list[i].setBrush(state_element.color)
+        self.update_diagramm(None)
 
+    def timerEvent(self, event=None):
+        self.set_by_states()
         if (self.current_state == self.max_state):
-            self.current_state = 0
+            #self.current_state = 0
+            self.play_states_event.clear()
+            self.killTimer(self.current_change_state_timer)
         else:
             self.current_state += 1
-
-        self.update_elements(None)
+        self.states_slider.setValue(self.current_state)
 
     def change_play_states_wait_event(self):
         if (self.play_states_event.isSet()):
@@ -169,12 +187,35 @@ class SortWidget(AlgoWidget):
             self.killTimer(self.current_change_state_timer)
         else:
             self.play_states_event.set()
-            self.current_change_state_timer = self.startTimer(PAUSE_TIME)      
+            self.current_change_state_timer = self.startTimer(PAUSE_TIME)
 
-    def play_states(self):
-        while(True):
-            self.play_states_event.wait()
-            self.change_state_timer.start(PAUSE_TIME)
+    def slider_pressed(self):
+        if (self.play_states_event.isSet()):
+            self.killTimer(self.current_change_state_timer)
+        self.current_state = self.states_slider.value()
+        self.set_by_states()
+
+    def slider_released(self):
+        self.current_state = self.states_slider.value()
+        self.set_by_states()
+        if(self.play_states_event.isSet()):
+            self.current_change_state_timer = self.startTimer(PAUSE_TIME)
+
+    def slider_changed(self):
+        self.current_state = self.states_slider.value()
+        self.set_by_states()
+
+    def next_state(self):
+        if (self.current_state != self.max_state):
+            self.current_state += 1
+        self.states_slider.setValue(self.current_state)
+        self.set_by_states()
+
+    def prev_state(self):
+        if (self.current_state != 0):
+            self.current_state -= 1
+        self.states_slider.setValue(self.current_state)
+        self.set_by_states()
 
 
 class BubbleSort(SortWidget):
@@ -182,9 +223,6 @@ class BubbleSort(SortWidget):
         super().__init__(min_rand, max_rand, elements_count, parent)
         self.setWindowTitle(BUBBLE_SORT_STRING)
         self.set_description(BUBBLE_SORT_DESCR, BUBBLE_SORT_ALG)
-        self.set_states()
-        #self.sort_thread = threading.Thread(target=BubbleSort.bubble_sort, args=(self,))
-        #self.sort_thread.setDaemon(True)
 
     def set_states(self):
         self.states_list = [[StateElement(e.value, self.standard_color) for e in self.sort_list]]
@@ -214,8 +252,6 @@ class QuickSort(SortWidget):
         self.set_description(BUBBLE_SORT_DESCR, BUBBLE_SORT_ALG)
 
 class SortButton(QtWidgets.QPushButton):
-    __metaclass__ = ABCMeta
-    @abstractmethod
     def __init__(self, parent=None):
         super().__init__(parent)
         self.clicked.connect(self.clicked_method)
@@ -223,7 +259,6 @@ class SortButton(QtWidgets.QPushButton):
         self.max_rand = MAX_RAND
         self.elements_count = ELEMENTS_COUNT
 
-    @abstractmethod
     def clicked_method(self):
         pass
 
